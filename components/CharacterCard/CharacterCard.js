2470f4dff049b3a7cad0183de53f5f5b8b0e9a71
Sercan Sebil <sercansebil95@googlemail.com>
2024-07-17 11:40:28 +0200

    add ccharacter card function

--- components/CharacterCard/CharacterCard.js
+++ components/CharacterCard/CharacterCard.js
@@ -1,1 +1,27 @@
-export function CharacterCard() {}
+export function CharacterCard(image, status, name, type, occurences) {
+  const newListItem = document.createElement("li");
+  newListItem.classList.add("card");
+
+  newListItem.innerHTML = `
+<div class="card__image-container">
+            <img
+              class="card__image"
+              src="${image}"
+              alt="${name}"
+            />
+            <div class="card__image-gradient"></div>
+          </div>
+          <div class="card__content">
+            <h2 class="card__title">${name}</h2>
+            <dl class="card__info">
+              <dt class="card__info-title">${status}</dt>
+              <dd class="card__info-description">Alive</dd>
+              <dt class="card__info-title">Type</dt>
+              <dd class="card__info-description">${type}</dd>
+              <dt class="card__info-title">Occurrences</dt>
+              <dd class="card__info-description">${occurences}</dd>
+            </dl>
+          </div>
+`;
+  return newListItem;
+}
