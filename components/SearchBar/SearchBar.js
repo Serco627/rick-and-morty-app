b7b3d7751d6a30053bf414e81e94f77636f7624c
Sercan Sebil <sercansebil95@googlemail.com>
2024-07-17 14:52:10 +0200

    minor changes

--- components/SearchBar/SearchBar.js
+++ components/SearchBar/SearchBar.js
@@ -3,12 +3,11 @@
 import { fetchCharacters } from "../../index.js";
 
 const searchBar = document.querySelector('[data-js="search-bar"]');
-let searchQuery = "";
 
 searchBar.addEventListener("submit", async (event) => {
   event.preventDefault();
   const formData = new FormData(searchBar);
   const query = formData.get("query").trim();
-  searchQuery = query;
-  await fetchCharacters(1, searchQuery);
+
+  await fetchCharacters(query);
 });
