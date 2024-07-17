d3b26ce4c3dbcd4bf2374a1f70e4bd3984f08990
Ole Backhaus <ole.backhaus@gmx.de>
2024-07-17 12:19:35 +0200

    Build fetchCharacters function an import CharacterCard.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import CharacterCard from "./components/CharacterCard/CharacterCard.js";
+
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
@@ -13,4 +15,25 @@ const maxPage = 1;
 const page = 1;
 const searchQuery = "";
 
-function fetchCharacters() {}
+export async function fetchCharacters() {
+  // Fetch Anfrage an die API
+  const response = await fetch("https://rickandmortyapi.com/api/character/");
+  const data = await response.json();
+
+  // Extrahieren der ersten 20 Charaktere
+  const characters = data.results.slice(0, 20);
+
+  // Leeren des cardContainers
+  cardContainer.innerHTML = "";
+
+  // Erstelle und hinzufügen der Charakterkarten
+  characters.forEach((character) => {
+    const characterCard = CharacterCard(character);
+    cardContainer.appendChild(characterCard);
+  });
+
+  console.log(characters);
+}
+
+// Funktion aufrufen, um die Charaktere zu laden
+fetchCharacters();
